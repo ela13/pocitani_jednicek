cfc38d36ad25bedb73664aa2dc0f304bdcb120c2
Hana Schreiberová <hanaschreiberova@seznam.cz>
2017-10-30 19:34:49 +0100

    Pridavam program na pocitani jednicek.

---
+++ pocet_jednicek.py
@@ -0,0 +1,13 @@
+def pocet_jednicek(cislo):
+    pocet = 0
+    while cislo > 0:
+        zbytek = cislo % 10
+        if zbytek == 1:
+            pocet = pocet + 1 #Pocitadlo!!!!
+        print (zbytek)
+        cislo = cislo // 10
+    return pocet
+
+cislo = int(input("Zadej číslo:"))
+pocet = pocet_jednicek(cislo)
+print("Pocet jednicek v cisle {} je {}.".format(cislo, pocet))
